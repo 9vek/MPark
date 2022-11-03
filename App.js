b3ceacc74ebb722bcda700d13d03e3e76d8784a0
Kevyn <532775134@qq.com>
2022-11-03 15:36:00 +0800

    init

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  async getData() {
+    const resp = await fetch('https://ios-dev.shortcutsapi.com/parking-info/parking-info-macau-ios-dev')
+    const data = resp.json()
+    return data
+  }
+
+  render() {
+    // const data = await this.getData();
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.map}>
+        </MapView>
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +29,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
