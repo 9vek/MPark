bedd76f3c8842cf9faa6fe64efcf16b5bc6af4e2
Kevyn <532775134@qq.com>
2022-11-09 11:13:28 +0800

    integrated tw

--- App.js
+++ App.js
@@ -1,37 +1,53 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
-import { StyleSheet, View, Dimensions, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, FlatList, Text, View } from 'react-native';
+import { roundToNearestPixel } from 'react-native/Libraries/Utilities/PixelRatio';
 
-export default class App extends React.Component {
 
-  async getData() {
-    const resp = await fetch('https://ios-dev.shortcutsapi.com/parking-info/parking-info-macau-ios-dev')
-    const data = resp.json()
-    return data
-  }
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-  render() {
-    // const data = await this.getData();
+  useEffect(() => {
+    fetch('https://ios-dev.shortcutsapi.com/parking-info/parking-info-macau-ios-dev')
+      .then((response) => response.json())
+      .then((json) => setData(json.data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const renderItem = ({ item }) => {
     return (
-      <View style={styles.container}>
-        <MapView style={styles.map}>
-        </MapView>
-      </View>
-    );
+      <Text>
+        <Text style={styles.id_tag} className="bg-lime-600 text-white rounded-xl mx-1">{" " + item.id + " "}</Text>
+        <Text className="text-bold text-xl">{item.name}</Text> {"\n"}
+        <Text>{item.address}</Text>
+      </Text>
+    )
   }
 
-}
+  return (
+
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <Text>Loading...</Text> :
+        (<View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
+          <Text style={{ fontSize: 18, color: 'green', textAlign: 'center' }}>{data.title}</Text>
+          <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10 }}>Parkings:</Text>
+          <FlatList
+            className="block m-2 bg-stone-50 shadow rounded"
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={renderItem}
+          />
+        </View>
+        )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+  id_tag: {
+    padding: "2px",
+    marginRight: "10px",
+    borderRadius: "10px"
   },
 });
